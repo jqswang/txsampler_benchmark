05bdcc475e17d31507d6c916c1610c4ff6d6e363
jqswang <qwang06@email.wm.edu>
2018-11-21 22:23:25 +0000

    Updated bart

--- benchmark/bart/compile.sh
+++ benchmark/bart/compile.sh
@@ -8,7 +8,7 @@ fi
 if [ "$1" == "origin" ] || [ "$1" == "all" ]; then
 	make allclean
 	rm -rf build_origin
-	make && make install PREFIX=$(pwd)/build_origin &&\
+	make && make nufft && make install PREFIX=$(pwd)/build_origin &&\
 	mkdir -p data &&\
 	build_origin/bin/bart traj -x4096 -y4096 data/traj.ra &&\
 	build_origin/bin/bart phantom data/shepplogan.ra -x 4096
@@ -17,7 +17,7 @@ fi
 if [ "$1" == "rtm" ] || [ "$1" == "all" ]; then
 	make allclean
 	rm -rf build_rtm
-	make RTM=yes &&\
+	make RTM=yes && make nufft RTM=yes &&\
 	make install RTM=yes PREFIX=$(pwd)/build_rtm &&\
 	mkdir -p data &&\
         build_rtm/bin/bart traj -x4096 -y4096 data/traj.ra &&\
