1e38bfffa37e634ccc2f5c4ba8f9da8770f3aa44
jqswang <qwang06@email.wm.edu>
2018-11-26 22:14:16 +0000

    Fixed some errors

--- script/measure_overhead.py
+++ script/measure_overhead.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 import argparse
 import ConfigParser
+import collections
 import csv
 import math
 import numpy as np
@@ -19,13 +20,17 @@ txsampler_err_list = []
 def execute_command(command):
 	if g_vars["verbose"]:
 		print("EXECUTE: {}".format(command))
-	out, err = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+	for _ in range(1):
+		child = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		out, err = child.communicate()
+		if child.returncode >= 0:
+			break
 	return out,err
 
 def read_info_file(file_path):
 	if g_vars["verbose"]:
 		print("Start to read {}".format(file_path))
-	apps = {}
+	apps = collections.OrderedDict()
 	with open(file_path) as f:
 		reader = csv.DictReader(f)
 		for row in reader:
@@ -46,13 +51,14 @@ def average_std(values):
 def measure_time(config, iterations, is_native):
 	time_list = []
 	if is_native:
-		cmd = "run-benchmark.py -r native -t {} -c {} {}".format(g_vars["num_threads"], g_vars["cpu_list"], config["run_file"])
+		cmd = "run-benchmark.py -r native -t {} -c {} {} --show-output".format(g_vars["num_threads"], g_vars["cpu_list"], config["run_file"])
 	else:
 		cmd = "run-benchmark.py -r txsampler -t {} -c {} {}".format(g_vars["num_threads"], g_vars["cpu_list"], config["run_file"])
 	for _ in range(iterations):
-		out, _ = execute_command(cmd)
+		out, err = execute_command(cmd)
 		try:
-			#print(out)
+			print(out)
+			print(err)
 			time = float(filter(lambda x:x.find("##MEASUREMENT:")>=0, out.split("\n"))[0].split()[-1])
 		except ValueError:
 			time = float('nan')
@@ -67,6 +73,7 @@ def measure_app(name, config, iterations):
 	if g_vars["verbose"]:
 		print("Change working directory to {}".format(working_dir))
 	os.chdir(working_dir)
+	print(config["compile_command"])
 	execute_command(config["compile_command"])
 	
 	# run
@@ -122,7 +129,7 @@ def main():
 		names = apps.keys()
 	else:
 		names = map(lambda x: x.lower(), args.applications.split(","))
-	
+
 	if any([name not in apps.keys() for name in names]):
 		print("Please specify an application or use 'all'")
 		return
