998f96850d507c783424a4c4e0fc367a3e059fb2
jqswang <qwang06@email.wm.edu>
2018-12-27 03:47:00 +0000

    Passed dedup Cleaned parsec-2.1

--- benchmark/parsec-2.1/common_rule.mk
+++ benchmark/parsec-2.1/common_rule.mk
@@ -8,39 +8,6 @@ ifeq (, $(wildcard ${PARSEC_ROOT}/pkgs/lib/ssl/libssl.a))
 	make -j AR_BIN=ar RANLIB=ranlib
 endif
 
-gsl :
-ifeq (, $(wildcard ${PARSEC_ROOT}/pkgs/lib/install/include/gsl/gsl_rng.h))
-	cd ${PARSEC_ROOT}/pkgs/lib/gsl;\
-	chmod u+x configure;\
-	./configure --prefix=${PARSEC_ROOT}/pkgs/lib/install;\
-	make -j && make install
-endif
-
-imagick : zlib jpeg
-ifeq (, $(wildcard ${PARSEC_ROOT}/pkgs/lib/install/include/Magick++.h))
-	cd ${PARSEC_ROOT}/pkgs/lib/imagick;\
-	chmod u+x configure;\
-	./configure --without-perl --prefix=${PARSEC_ROOT}/pkgs/lib/install LDFLAGS=-L${PARSEC_ROOT}/pkgs/lib/install/lib CPPFLAGS=-I${PARSEC_ROOT}/pkgs/lib/install/include;\
-	make -j && make install
-endif
-
-jpeg : 
-ifeq (, $(wildcard ${PARSEC_ROOT}/pkgs/lib/install/include/jpeglib.h))
-	cd ${PARSEC_ROOT}/pkgs/lib/jpeg-8c;\
-	chmod u+x configure;\
-	./configure --prefix=${PARSEC_ROOT}/pkgs/lib/install;\
-	make -j && make install
-endif
-
-zlib :
-ifeq (, $(wildcard ${PARSEC_ROOT}/pkgs/lib/install/include/zlib.h))
-	cd ${PARSEC_ROOT}/pkgs/lib/zlib;\
-	chmod u+x configure;\
-	./configure --prefix=${PARSEC_ROOT}/pkgs/lib/install;\
-	make -j && make install
-endif
-
-
 librtm.so :
 ifeq (, $(wildcard $(TM_DIR)/librtm.so))
 	cd $(TM_DIR); make -C $(TM_DIR) CC=$(CC)
