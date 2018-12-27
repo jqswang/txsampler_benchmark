023db0cc9b4324145643e6ae4a3140a7e5b3599d
jqswang <qwang06@email.wm.edu>
2018-12-26 20:25:50 -0500

    Modified scripts

--- script/launch_applications.py
+++ script/launch_applications.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python2
 import argparse
-import ConfigParser
+import collections
 import csv
 import os
 import shlex
 import subprocess
 
 g_vars = {}
+
 
 def execute_command(command):
 	if g_vars["verbose"]:
@@ -15,11 +16,10 @@ def execute_command(command):
 	out, err = child.communicate()
 	return out,err
 
-
 def read_info_file(file_path):
 	if g_vars["verbose"]:
 		print("Start to read {}".format(file_path))
-	apps = {}
+	apps = collections.OrderedDict()
 	with open(file_path) as f:
 		reader = csv.DictReader(f)
 		for row in reader:
@@ -30,35 +30,27 @@ def read_info_file(file_path):
 				config[field] = row[field]
 	return apps
 
-def run_application(name, config):
+
+def run_app(name, config):
+
 	# compile
 	working_dir = os.path.join(os.environ["TSX_ROOT"], "benchmark", config["compile_path"])
 	if g_vars["verbose"]:
 		print("Change working directory to {}".format(working_dir))
 	os.chdir(working_dir)
+	print(config["compile_command"])
 	execute_command(config["compile_command"])
-
+	
 	# run
 	working_dir = os.path.join(os.environ["TSX_ROOT"], "benchmark", config["run_path"])
 	if g_vars["verbose"]:
 		print("Change working directory to {}".format(working_dir))
 	os.chdir(working_dir)
-	out, err = execute_command("run-benchmark.py -r txsampler --show-output --verbose -t {} -c {} {}".format(g_vars["num_threads"], g_vars["cpu_list"], config["run_file"]))
 
-	#print(out)
-	#print(err)	
-	lines = out.split("\n")
-	try:
-		time = float(filter(lambda x:x.find("##MEASUREMENT:")>=0, out.split("\n"))[0].split()[-1])
-	except ValueError:
-		time = float('nan')
-	
-	print("Running {} takes {}s".format(name, round(time,2)))
-	try:
-		database_dir = filter(lambda x:x.find("msg: Populating Experiment database:")>=0, lines)[0].split()[-1]
-		print("Profile database of {} generated: {}".format(name, database_dir))
-	except IndexError:
-		print("Failed to generate the profile database of {}".format(name))
+	cmd = "run-benchmark.py -t {} {} --show-output".format(g_vars["num_threads"], config["run_file"])
+	out, err = execute_command(cmd)
+	print(out)
+	print(err)
 
 def main():
 	global g_vars
@@ -66,14 +58,16 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("applications", nargs='?', default=None, help="the name list of the applications (separated by ,), or \"all\" for all the available applications")
 	parser.add_argument("-l", "--list", action='store_true', help="list all available applications")
+	parser.add_argument("-t", "--threads", type=int, default=12, help="the number of threads the launched program will run with (not applicable to PARSEC")
 	parser.add_argument("--verbose", action='store_true', help="List every step")
 	args = parser.parse_args()
-	
 
 	g_vars["verbose"] = args.verbose
+	g_vars["num_threads"] = args.threads
 
 	# read info file
-	apps = read_info_file(os.path.join(os.path.dirname(os.path.realpath(__file__)), "casestudy-list.csv"))
+	apps = read_info_file(os.path.join(os.path.dirname(os.path.realpath(__file__)), "app-list.csv"))
+
 	if args.list:
 		print("Available applications:\n")
 		print("\n".join(apps.keys()))
@@ -91,26 +85,10 @@ def main():
 	if any([name not in apps.keys() for name in names]):
 		print("Please specify an application or use 'all'")
 		return
-	
-	# resolve other config information
 
-	config = ConfigParser.ConfigParser()
-	config.optionxform = str
-	config.read(os.path.join(os.environ["TSX_ROOT"], "run.conf"))
-	if not config.has_section("running"):
-		print("Error: The file run.conf is not correctly configured.")
-		return
 
-	if not config.has_option("running", "num_threads"):
-		print("Error: No num_threads in run.conf")
-		return
-	g_vars["num_threads"] = config.get("running", "num_threads")
-	if not config.has_option("running", "cpu_list"):
-		print("Error: No cpu_list in run.conf")
-		return
-	g_vars["cpu_list"] = config.get("running", "cpu_list")
-	
 	for name in names:
-		run_application(name, apps[name])
-	
+		run_app(name, apps[name])
+		
+		
 main()
--- script/run-benchmark.py
+++ script/run-benchmark.py
@@ -8,7 +8,6 @@ from optparse import OptionParser
 
 g_vars = {}
 
-g_vars["event_code_map"] = {"1":("cycles:precise=2", 1000000),"2": ("RTM_RETIRED:ABORTED",10000),"3": ("RTM_RETIRED:COMMIT",3000 ),"4": ("MEM_UOPS_RETIRED:ALL_STORES", 100000)}
 
 def log_print(s):
 	if g_vars["verbose"]:
@@ -22,42 +21,14 @@ def execute_command(command, shell_flag=False):
 	out,err = child.communicate()
 	return out, err, child.returncode
 
-def time_execute(prefix_cmd, main_cmd, shell_flag = False):
-	time_file_name = "/tmp/measured_time.tmp"
-	time_cmd = "/usr/bin/time -f %e -o "+time_file_name
-	if g_vars["use_script"]:
-		os.environ["TXSAMPLER_CMD"] = time_cmd + " " + prefix_cmd
-		cmd = main_cmd
-	else:
-		cmd = " ".join([time_cmd,prefix_cmd,main_cmd])
-	try:
-		os.remove(time_file_name)
-	except OSError:
-		pass
-	out,err, ret_code = execute_command(cmd, shell_flag)
-	if os.path.exists(time_file_name):
-		with open(time_file_name) as f:
-			try:
-				time = f.readlines()[-1].strip().rstrip().split()[-1]
-			except IndexError:
-				time = 0
-		os.remove(time_file_name)
-	else:
-		time = 0
-	#print(time)
-	return out, err, float(time), ret_code
 
 def parse_arguments():
         global g_vars
         usage = "usage: %prog [options] [run_file,default.run]"
         parser = OptionParser(usage)
-        parser.add_option("-i", type="int", dest="iteration", default=1, help="The number of iterations.")
-        parser.add_option("-r", type="string", dest="run", default="none", help="Available option native|txsampler")
-        parser.add_option("-e", type="string", dest="events", default="all", help="What events to sample. 1(cycles),2(aborts),3(commits),4(stores), all(all-above). E.g. 1,2,3,4")
         parser.add_option("--show-output", action="store_true", dest="show_output", default=False, help="Whether to show the launched program's output")
         parser.add_option("--verbose", action="store_true", dest="verbose", default=False, help="Print every command executed")
 	parser.add_option("-t", type="int", dest="num_threads", default=1, help="The number of threads to launch the program if configurable (not applicable to PARSEC)")
-	parser.add_option("-c", type="string", dest="cpu_list", default="0", help="The CPUs the program is bound to. It is similar to --cpu-list in taskset")
 	parser.add_option("--dry-run", action="store_true", dest="dry_run", default=False, help="Force dry run")
 
 	(options, args) =parser.parse_args()
@@ -66,13 +37,9 @@ def parse_arguments():
 	else:
 		g_vars["run_file"] = args[0]
 
-	g_vars["iterations"] = options.iteration
-	g_vars["run_type"] = options.run
-	g_vars["event_code"] = options.events
 	g_vars["show_output"] = options.show_output
 	g_vars["verbose"] = options.verbose or options.dry_run
 	g_vars["num_threads"] = options.num_threads
-	g_vars["cpu_list"] = options.cpu_list
 	g_vars["dry_run"] = options.dry_run
 
 def strip_quotes(s):
@@ -108,66 +75,13 @@ def parse_run_file():
 	g_vars["app_shown_name"] = config.get("information", "shown_name")
 	g_vars["args"] = fill_environment_variables(strip_quotes(config.get("information", "arguments")))
 
-	g_vars["sampling_dict"] = {}
-	if config.has_section("sampling"):
-		for (key, value) in config.items("sampling"):
-			key = key.replace("?",":")
-			key = key.replace("--", "=")
-			g_vars["sampling_dict"][key] = value
 	
-def native_run(prefix_cmd, main_cmd):
-	for i in range(1, g_vars["iterations"]+1):
-		for _ in range(3):
-			out, err, time, ret_code = time_execute(prefix_cmd, main_cmd)
-			if ret_code >= 0:
-				break
-		if g_vars["show_output"]:
-			print(out)
-			print(err)
-		print("##MEASUREMENT: native {} {} {}".format(g_vars["app_shown_name"], i, round(time,2)))
-			
-def get_event_list(event_code):
-	ret_list = []
-	event_code = event_code.replace("all","1,2,3,4")
-	for e in event_code.split(","):
-		assert e in g_vars["event_code_map"]
-		ret_list.append(g_vars["event_code_map"][e])
-	return ret_list	
-			
+def run(cmd):
+	out, err, _ = execute_command(cmd)
+	if g_vars["show_output"]:
+		print(out)
+		print(err)
 
-def txsampler_run(prefix_cmd, main_cmd):
-	event_list = get_event_list(g_vars["event_code"])
-	if "sampling_dict" in g_vars:
-		new_event_list = []
-		for name,interval in event_list:
-			if name in g_vars["sampling_dict"]:
-				interval = int(g_vars["sampling_dict"][name])
-			new_event_list.append((name, interval))
-		event_list = new_event_list
-	event_str = " ".join(["-e " + name + "@" + str(interval) for name,interval in event_list])
-
-	for i in range(1, g_vars["iterations"]+1):
-		exe = g_vars["exe"]
-		exe_base = os.path.basename(exe)
-		log_print("Cleaning *.hpcstruct hpctoolkit-*")
-		subprocess.Popen("%s %s" % ("rm", "-rf *.hpcstruct hpctoolkit-*"),shell=True).wait()
-		out, err, _  = execute_command("hpcstruct "+ exe)
-		if out and g_vars["show_output"]:
-			print(out)
-		if err and g_vars["show_output"]:
-			print(err)
-		measurement_folder = "hpctoolkit-"+ exe_base+"-measurements"
-		out, err, time, ret_code = time_execute(" ".join([prefix_cmd, "hpcrun", event_str]), main_cmd)
-		if out and g_vars["show_output"]:
-			print(out)
-		if err and g_vars["show_output"]:
-			print(err)
-		out, err, _ = execute_command("hpcprof-mpi -o hpctoolkit-"+ exe_base+"-database -S ./"+exe_base+".hpcstruct -I ../ "+ measurement_folder)
-                if out and g_vars["show_output"]:
-                        print(out)
-                if err and g_vars["show_output"]:
-                        print(err)
-		print("##MEASUREMENT: txsampler {} {} {}".format(g_vars["app_shown_name"], i, round(time,2)))
 
 def main():
 	parse_arguments()
@@ -189,24 +103,17 @@ def main():
 
 	# construct running cmd
         if g_vars["use_script"]:
-		main_cmd = g_vars["launch_script"]
+		cmd = g_vars["launch_script"]
         else:
-                main_cmd = g_vars["exe"]
+                cmd = g_vars["exe"]
 
-        if not os.path.isabs(main_cmd):
-		main_cmd = os.path.join(os.getcwd(),main_cmd)
+        if not os.path.isabs(cmd):
+		cmd = os.path.join(os.getcwd(),cmd)
 	if not g_vars["use_script"]:
-		main_cmd = main_cmd+ " "+g_vars["args"]
+		cmd = cmd+ " "+g_vars["args"]
 
-	# construct prefix
-	prefix_cmd = "taskset -c " + g_vars["cpu_list"]
-
-	# native run
-	if g_vars["run_type"] == "native":
-		native_run(prefix_cmd, main_cmd)
-	# txsampler run
-	if g_vars["run_type"] == "txsampler":
-		txsampler_run(prefix_cmd, main_cmd)
+	# run
+	run(cmd)
 
 
 main()
