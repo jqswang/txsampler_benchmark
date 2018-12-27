873fa7edc08ec2bc31d9273e1bc91ff1f12f3e40
jqswang <qwang06@email.wm.edu>
2018-12-27 04:16:53 +0000

    Passed netferret

--- benchmark/parsec-3.0/pkgs/netapps/netferret/run-hpc.sh
+++ benchmark/parsec-3.0/pkgs/netapps/netferret/run-hpc.sh
@@ -75,7 +75,7 @@ killall -9 server
 
 #Execution
 if [ -z "$NETMODE" ]; then
-  RUN_SERVER="${TXSAMPLER_CMD} $PROG_PATH/server $PROGARGS_SERVER"
+  RUN_SERVER="$PROG_PATH/server $PROGARGS_SERVER"
   RUN_CLIENT="$PROG_PATH/client $PROGARGS_CLIENT &"
 
   echo "Running"
